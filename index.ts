832e9c4ba72139801edbd76afd921f4db88d6e29
alan <alan>
2024-03-29 01:11:12 -0400

    added envs

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
+import express, { Express, Request, Response, Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import express, { Express, Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+//For env File
+dotenv.config({ path:__dirname+ `/.env.${process.env.NODE_ENV}` });
 
-//For env File 
-dotenv.config();
+const nodeEnv = process.env.NODE_ENV;
+const port = process.env.PORT;
+const environment = process.env.ENVIRONMENT;
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server");
 });
 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use("/api/v1/", router);
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(
+    `==> start listening *** port: #${port} *** node_env: ${nodeEnv} *** environment: ${environment}`
+  );
 });
